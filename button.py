4cc401587e3e954d7bbd67f158948f1bacde7eac
Najum Hussain <najum_lhr@hotmail.com>
2025-06-28 16:38:17 +1000

    progress

--- button.py
+++ button.py
@@ -3,17 +3,19 @@ import time
 
 # GPIO pin 16 (physical pin 36 on Pi Zero 2 W header)
 LED_PIN = 16
+BUTTON_PIN = 15
 
 GPIO.setmode(GPIO.BCM)       # Use Broadcom GPIO numbering
 GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button with pull-up resistor
 
 try:
     while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)  # LED ON
-        time.sleep(2)
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            GPIO.output(LED_PIN, GPIO.HIGH)  # LED ON
+        else:    
+            GPIO.output(LED_PIN, GPIO.LOW)   # LED OFF
 
-        GPIO.output(LED_PIN, GPIO.LOW)   # LED OFF
-        time.sleep(2)
 except KeyboardInterrupt:
     pass
 finally:
